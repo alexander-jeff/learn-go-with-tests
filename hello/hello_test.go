0706671d5ca04d4f4b0e4fd030f7fcca1e90dc1b
Jeff Alexander <12126499+alexander-jeff@users.noreply.github.com>
2022-04-28 14:56:09 -0700

    Migrate to stretchr/testify assertion library

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,31 +1,30 @@
 package hello
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestHello(t *testing.T) {
-	assertCorrectMessage := func(t testing.TB, got, want string) {
-		t.Helper()
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	}
+	assert := assert.New(t)
 
 	t.Run("can greet a name", func(t *testing.T) {
 		got := Hello("Alex", "")
 		want := "Hello, Alex"
-		assertCorrectMessage(t, got, want)
+		assert.Equal(got, want)
 	})
 
 	t.Run("greets world by default", func(t *testing.T) {
 		got := Hello("", "")
 		want := "Hello, World!"
-		assertCorrectMessage(t, got, want)
+		assert.Equal(got, want)
 	})
 
 	t.Run("in Spanish", func(t *testing.T) {
 		got := Hello("Elodie", SPANISH)
 		want := "Hola, Elodie"
-		assertCorrectMessage(t, got, want)
+		assert.Equal(got, want)
 	})
 
 	t.Run("in French", func(t *testing.T) {
@@ -34,7 +33,7 @@ func TestHello(t *testing.T) {
 			FRENCH,
 		)
 		want := "Bonjour, Jean Luc"
-		assertCorrectMessage(t, got, want)
+		assert.Equal(got, want)
 	})
 
 	t.Run("in Hawaiian", func(t *testing.T) {
@@ -43,6 +42,6 @@ func TestHello(t *testing.T) {
 			HAWAIIAN,
 		)
 		want := "Aloha, Jean Luc"
-		assertCorrectMessage(t, got, want)
+		assert.Equal(got, want)
 	})
 }
