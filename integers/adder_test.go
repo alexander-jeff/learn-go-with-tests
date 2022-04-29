724c13826ef1c5c6c4502775cb95823ad6c858d2
Jeff Alexander <12126499+alexander-jeff@users.noreply.github.com>
2022-04-29 12:45:36 -0700

    Complete integer section

---
+++ integers/adder_test.go
@@ -0,0 +1,20 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+	assert.Equal(t, expected, sum)
+}
